30d05474fb1f622fce0fcad44b196c11728c3457
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2023-12-26 15:55:59 +0700

    Tao danh sach san pham client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,7 +1,22 @@
+const Product = require("../../models/product.model");
+
 //[GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    for (const item of products) {
+        item.newPrice = item.price * (1 - item.discountPercentage / 100);
+        item.newPrice = item.newPrice.toFixed(0);
+    }
+
+    //console.log(products);
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sach san pham"
+        pageTitle: "Danh sach san pham",
+        products: products
     });
 }
 
