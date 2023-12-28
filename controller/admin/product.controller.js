e0e0130ccf4a7ec3a44d6f7372baae4b51f6898b
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2023-12-29 00:40:59 +0700

    Tao bo loc

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,5 +1,45 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const filterState = [
+        {
+          name: "Tất cả",
+          status: "",
+          class: ""
+        },
+        {
+          name: "Hoạt động",
+          status: "active",
+          class: ""
+        },
+        {
+          name: "Dừng hoạt động",
+          status: "inactive",
+          class: ""
+        }
+    ];
+
+    if(req.query.status){
+        const index = filterState.findIndex(item => item.status == req.query.status);
+        filterState[index].class = "active";
+    } else {
+        filterState[0].class = "active";
+    }
+
+    const find = {
+        deleted: false,
+    }
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+    //console.log(products);
+
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang tong quan san pham"
+        pageTitle: "Trang tong quan san pham",
+        products: products,
+        filterState: filterState
     });
 };
