f150c6dddb0556ba97c930ac4bda8e7c8935cf75
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2023-12-29 13:17:33 +0700

    Tinh nang tim kiem

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -34,12 +34,18 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    if(req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
     //console.log(products);
 
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang tong quan san pham",
         products: products,
-        filterState: filterState
+        filterState: filterState,
+        keyword: req.query.keyword
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -6,7 +6,6 @@ if(buttonsStatus.length > 0) {
   buttonsStatus.forEach((button) => {
     button.addEventListener("click", () => {
       const status = button.getAttribute("button-status");
-      console.log(status);
       if(status) {
         url.searchParams.set("status", status);
       } else {
@@ -18,3 +17,25 @@ if(buttonsStatus.length > 0) {
   });
 }
 // End Button Status
+
+//Form Search
+
+const formSearch = document.querySelector("#form-search");
+if(formSearch){
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const keyword = event.target.elements.keyword.value;
+
+    if(keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+
+    window.location.href = url.href;
+  });
+}
+
+//End Form Search
