bb8b8e0cfd93fa1405d550f35896e50f6e8c9b4f
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-01-09 16:08:57 +0700

    Preview anh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -192,3 +192,18 @@ if(showAlert){
   });
 }
 //End Show Alert
+
+//Preview Image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage){
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");;
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (event) => {
+    const [file] = uploadImageInput.files;
+    if(file){
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });  
+}
+//End Preview Image
