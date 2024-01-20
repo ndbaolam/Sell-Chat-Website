b156a9ddbb562bfd6709018da1ec2684709e6610
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-01-20 20:10:07 +0700

    3.3. Xây dựng phần Thêm mới nhóm quyền

--- controller/admin/role.controller.js
+++ controller/admin/role.controller.js
@@ -1,6 +1,30 @@
+const Role = require('../../models/role.model');
+
+const systemConfig = require("../../config/system");
+
 // [GET] /admin/roles/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const records = await Role.find({
+        deleted: false
+    });
+
     res.render("admin/pages/roles/index", {
+      pageTitle: "Nhóm quyền",
+      records: records
+    });
+}
+
+// [GET] /admin/create/
+module.exports.create = (req, res) => {
+    res.render("admin/pages/roles/create", {
       pageTitle: "Nhóm quyền"
     });
+}
+
+// [POST] /admin/create/
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
