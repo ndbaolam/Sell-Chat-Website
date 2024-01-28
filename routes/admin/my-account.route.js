4668a00ab0cb438a414acd2a0dd8535992b1e303
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-01-28 20:57:26 +0700

    1. Làm trang Thông tin cá nhân bên admin (Chỉnh sửa thông tin cá nhân)

--- routes/admin/my-account.route.js
+++ routes/admin/my-account.route.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 const controller = require('../../controller/admin/my-account.controller');
 
 router.get('/', controller.index);
+
+router.get("/edit", controller.edit);
+
+router.patch(
+  "/edit",
+  upload.single("avatar"),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+);
 
 module.exports = router;
