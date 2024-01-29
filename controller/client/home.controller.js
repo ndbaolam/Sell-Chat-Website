3f5b67cec764b925437b37c46814419a56804205
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-01-29 15:09:10 +0700

    6. Trang chi tiết sản phẩm bên client (chưa làm phần 5. Trang danh sách sản phẩm bên client (Lấy cả các sản phẩm thuộc danh mục con)

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -8,11 +8,23 @@ module.exports.index = async (req, res) => {
     }).sort({ position: "desc"}).limit(6);
 
     for (const item of productsFeatured) {
-        item.priceNew = item.price * (100 - item.discountPercentage)/100;
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
     }
+
+    // Sản phẩm mới nhất
+    const productsNew = await Product.find({
+        status: "active",
+        deleted: false,
+    }).sort({ position: "desc" }).limit(6);
+
+    for (const item of productsNew) {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+    }
+    // Hết Sản phẩm mới nhất
 
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang chu",
-        productsFeatured: productsFeatured
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 };
--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require('../../models/product-category.model');
 
 //[GET] /products/
 module.exports.index = async (req, res) => {
@@ -22,17 +23,27 @@ module.exports.index = async (req, res) => {
     });
 }
 
-// [GET] /products/:slug
+// [GET] /products/:slugProduct
 module.exports.detail = async (req, res) => {
     try {
-      const slug = req.params.slug;
+      const slug = req.params.slugProduct;
   
       const product = await Product.findOne({
         slug: slug,
         deleted: false,
         status: "active"
       });
-  
+      
+      product.priceNew = (product.price * (100 - product.discountPercentage)/100).toFixed(0);
+
+      if(product.product_category_id) {
+        const category = await ProductCategory.findOne({
+          _id: product.product_category_id
+        });
+
+        product.category = category;
+      }
+
       console.log(product);
   
       res.render("client/pages/products/detail", {
@@ -42,4 +53,32 @@ module.exports.detail = async (req, res) => {
     } catch (error) {
       res.redirect("/");
     }
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory  = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    status: "active",
+    deleted: false
+  });
+
+  const products = await Product.find({
+    product_category_id: category.id,
+    status: "active",
+    deleted: false
+  }).sort({ position: "desc" });
+
+  for (const item of products) {
+    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+  }
+
+  console.log(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products
+  });
 }
