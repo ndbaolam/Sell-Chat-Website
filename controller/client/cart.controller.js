9747de59b8d7ecbfc16ab2ec1d68b980ae3abf1b
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-02-05 02:52:11 +0700

    2.5. Cập nhật số lượng sản phẩm trong giỏ hàng

--- controller/client/cart.controller.js
+++ controller/client/cart.controller.js
@@ -86,6 +86,27 @@ module.exports.delete = async (req, res) => {
     });
 
     req.flash('success', 'Đã xóa sản phẩm khỏi giỏ hàng!');
-    
+
+    res.redirect('back');
+}
+
+// [GET] /update/:productId/:quantity
+module.exports.update = async (req, res) => {
+    try {
+        const cartId = req.cookies.cartId;
+        const productId = req.params.productId;
+        const quantity = req.params.quantity;
+
+        await Cart.updateOne({
+            _id: cartId,
+            "products.product_id": productId
+        }, {
+            $set: { "products.$.quantity": quantity }
+        });
+
+        req.flash("success", "Cập nhật số lượng thành công!");
+    } catch (error) {
+        req.flash('error', 'Cập nhật số lượng không thành công!');
+    }
     res.redirect('back');
 }
