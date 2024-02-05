ea76146de575883f31b2dd550f08fac97993c7b5
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-02-06 00:30:08 +0700

    2.2. Đăng ký tài khoản

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,7 @@ const productRoutes = require("./product.route")
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const userRoutes = require('./user.route');
 const checkoutRoutes = require("./checkout.route");
 
 const categoryMiddleware = require('../../middlewares/client/category.middleware');
@@ -20,4 +21,6 @@ module.exports = (app) => {
     app.use("/cart", cartRoutes);
 
     app.use("/checkout", checkoutRoutes);
+
+    app.use("/user", userRoutes);
 }
