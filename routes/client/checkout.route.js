199e8ccc92585f648eddfdfb474c1de98604a1b5
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-02-05 22:58:44 +0700

    1.3. Lấy data được gửi lên lưu vào database

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -5,4 +5,6 @@ const controller = require('../../controller/client/checkout.controller');
 
 router.get('/', controller.index);
 
+router.post('/order', controller.order);
+
 module.exports = router;
