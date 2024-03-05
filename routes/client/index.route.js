1d53fe721262a990f0ee8fd79445bea28a70fb87
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-05 18:22:41 +0700

    1.1. Hiển thị danh sách user

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,7 @@ const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const userRoutes = require('./user.route');
+const usersRoutes = require('./users.route');
 const chatRoutes = require('./chat.route');
 const checkoutRoutes = require("./checkout.route");
 
@@ -29,4 +30,6 @@ module.exports = (app) => {
     app.use("/user", userRoutes);
 
     app.use('/chat', authMiddleware.requireAuth, chatRoutes);
+
+    app.use('/users', authMiddleware.requireAuth, usersRoutes);
 }
