11423a1fecd304bea287b2f6c2b0bf287578c98c
ndbaolam <135936837+ndbaolam@users.noreply.github.com>
2024-03-07 22:05:08 +0700

    1.3. Hiển thị danh sách tin nhắn theo phòng chat

--- routes/client/chat.route.js
+++ routes/client/chat.route.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controller/client/chat.controller");
+const chatMiddleware = require('../../middlewares/client/chat.middleware');
 
-router.get('/:roomChatId', controller.index);
+router.get(
+    '/:roomChatId',
+    chatMiddleware.isAccess,
+    controller.index
+);
 
 module.exports = router;
--- sockets/client/chat.socket.js
+++ sockets/client/chat.socket.js
@@ -1,42 +1,46 @@
 const Chat = require("../../models/chat.model");
 const uploadToCloudinary = require("../../helpers/upload-to-cloudinary.helper");
 
-module.exports = (res) => {
+module.exports = (req, res) => {
     const userId = res.locals.user.id;
     const fullName = res.locals.user.fullName;
+    const roomChatId = req.params.roomChatId;
 
     // SocketIO
     _io.once("connection", (socket) => {
+        socket.join(roomChatId);
+
         // Người dùng gửi tin nhắn lên server
         socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-        const images = [];
+            const images = [];
 
-        for (const image of data.images) {
-            const url = await uploadToCloudinary(image);
-            images.push(url);
-        }
+            for (const image of data.images) {
+                const url = await uploadToCloudinary(image);
+                images.push(url);
+            }
 
-        // Lưu data vào database
-        const chat = new Chat({
-            user_id: userId,
-            content: data.content,
-            images: images
-        });
+            // Lưu data vào database
+            const chat = new Chat({
+                user_id: userId,
+                room_chat_id: roomChatId,
+                content: data.content,
+                images: images
+            });
 
-        await chat.save();
+            await chat.save();
 
-        // Trả data ra giao diện realtime
-        _io.emit("SERVER_SEND_MESSAGE", {
-            userId: userId,
-            fullName: fullName,
-            content: data.content,
-            images: images
-        });
+            // Trả data ra giao diện realtime
+            _io.to(roomChatId).emit("SERVER_SEND_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: data.content,
+                images: images
+            });
         });
 
         // Typing
         socket.on("CLIENT_SEND_TYPING", (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+            socket.broadcast.to(roomChatId).emit("SERVER_RETURN_TYPING", {
                 userId: userId,
                 fullName: fullName,
                 type: type
